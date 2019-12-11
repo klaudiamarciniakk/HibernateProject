4e93aedb29fa7c1461dc1fdd370ab7a8ba95f74b
mkielarv2 <mkielarv2@gmail.com>
2019-12-11 22:14:55 +0100

    connecting to db

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,23 @@
+import java.sql.*;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://balarama.db.elephantsql.com:5432", "yoqomwav", "aKalGO8OkfFFwOITn8rnJTe6EwrJUc3x")) {
+            System.out.println("Java JDBC PostgreSQL Example");
+
+            System.out.println("Connected to PostgreSQL database!");
+            Statement statement = connection.createStatement();
+            System.out.println("Reading movie records...");
+            System.out.printf("%-30.30s %-30.30s %-30.30s %-30.30s %-30.30s %-30.30s %-30.30s\n", "id_film", "nazwa", "cena", "gatunek_id", "opis", "zawiastun_id","rok_wydania");
+            ResultSet resultSet = statement.executeQuery("select * from yoqomwav.movie_rental.film");
+            while (resultSet.next()) {
+                System.out.printf("%-30.30s %-30.30s %-30.30s %-30.30s %-30.30s %-30.30s %-30.30s\n", resultSet.getString("id_filmu"), resultSet.getString("nazwa"),
+                        resultSet.getString("cena"), resultSet.getString("gatunek_id"), resultSet.getString("opis"), resultSet.getString("zwiastun_id"),  resultSet.getString("rok_wydania"));
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Connection failure.");
+            e.printStackTrace();
+        }
     }
 }
